909d12f2d9c4400576cfab4773faa5b381f3ea19
Shivani Katiyar <68283885+shivanikatiyar@users.noreply.github.com>
2020-10-15 21:40:40 +0530

    Loop using

---
+++ Loop use .py
@@ -0,0 +1,11 @@
+for i in range(10):
+    print("Hi how are you")
+
+for i in range(5):
+    print(i)
+    
+for i in range(10):
+    print("Hi, how are you",i)
+    
+for i in range(11):
+    print(i*2)
